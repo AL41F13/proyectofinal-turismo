c8806a75fce9bfcab828f25adff764a45ed28cc3
AL41F13 <janeth982210@gmail.com>
2015-06-25 10:37:15 -0500

    seis
    cambio

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,12 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
+//document.addEventListener("deviceready",function(){
+	$('#menu').on ('tap', function(){
+		navigator.vibrate(1000);
+	});
+	$('#intro').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
 }); 
-});
+//});
 
